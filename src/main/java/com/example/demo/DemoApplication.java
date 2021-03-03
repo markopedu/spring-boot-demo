a5801f68f391fa3e9ad986b9a41603627ad796ce
markopedu <69790644+markopedu@users.noreply.github.com>
2021-03-03 20:31:25 +0100

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public Greeting sayHello(@RequestParam(value="myName", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+
+
+}
