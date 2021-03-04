3c2aadbe1355d0ac7404cfcda62d168491d9d800
markopedu <69790644+markopedu@users.noreply.github.com>
2021-03-04 19:53:56 +0100

    add rest get time now

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicLong;
 
 @SpringBootApplication
@@ -22,6 +24,11 @@ public class DemoApplication {
     @GetMapping("/hello")
     public Greeting sayHello(@RequestParam(value="myName", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/time")
+    public TimeNow getTime() {
+        return new TimeNow(String.format("Time is now: %s", new SimpleDateFormat("yyyy-MM-dd").format(new Date())));
     }
 
 
---
+++ src/main/java/com/example/demo/TimeNow.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+public class TimeNow {
+    private String now;
+
+    public TimeNow(String now) {
+        this.now = now;
+    }
+
+    public String getNow() {
+        return now;
+    }
+
+    public void setNow(String now) {
+        this.now = now;
+    }
+}
